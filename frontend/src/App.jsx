710bdb23d5eddbad6849c9162d0fb34dd17502b3
ravirok <ravirok@users.noreply.github.com>
2026-03-20 09:02:30 +0530

    Update App.jsx

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,6 @@
 import React from "react";
 import Dashboard from "./components/Dashboard";
 
-function App() {
-  return (
-    <div>
-      <Dashboard />
-    </div>
-  );
+export default function App() {
+  return <Dashboard />;
 }
-
-export default App;
